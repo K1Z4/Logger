0de0f0be687f6455e36858082414ead64d9ff095
Kiza <github@ocdwasteland.com>
2020-01-13 22:43:31 +0000

    create folder if not exists

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 "use strict";
 const winston = require('winston');
 const kb300 = 300000;
+const fs = require('fs');
+
+const logsFolder = './logs';
+if (!fs.existsSync(logsFolder)){
+    fs.mkdirSync(logsFolder);
+}
 
 module.exports = new (winston.Logger)({
         transports: [
             new (winston.transports.Console)({ level: 'debug' }),
             new (winston.transports.File)({
                 name: "file#debug",
-                filename: './logs/debug.log',
+                filename: `${logsFolder}/debug.log`,
                 maxsize: kb300,
                 maxFiles: 3,
                 level: 'debug'
             }),
             new (winston.transports.File)({
                 name: "file#error",
-                filename: './logs/error.log',
+                filename: `${logsFolder}/error.log`,
                 maxsize: kb300,
                 maxFiles: 3,
                 level: 'error'
